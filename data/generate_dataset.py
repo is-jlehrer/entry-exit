c07e68000db8efeb03d7fed82a95ccf703ba2785
is-jlehrer <ec2-user@ip-172-31-1-125.ec2.internal>
2022-10-03 20:06:53 +0000

    Cleanup

--- data/generate_dataset.py
+++ data/generate_dataset.py
@@ -1,5 +1,4 @@
 from lightml.data.decomp import DecompFromDataFrame
-from lightml.common.defaults import download_from_uri
 
 import pathlib 
 import pandas as pd 
@@ -8,50 +7,23 @@ import concurrent.futures
 import argparse
 import tqdm 
 import boto3
+import datetime
+
 
 curr = pathlib.Path(__file__).parent.resolve()
 LOCAL_DOWNLOADED_VID_PATH = os.path.join(curr, "raw_data")
 
-def download_from_uri(uri, local_path):
-    # New client since these objects arent picklable and therefore 
-    # cannot be used with concurrent.futures
+def convert_to_ms(time):
+    # Time is formatted like: '1:03' or '01:03'
+    time = time.replace(".", ":").split(":")
+    m = int(time[0])
+    s = int(time[1])
+    ms = 1000 * (m*60 + s)
+    print(ms)
+    return 1000 * (m*60 + s)
 
-    client = boto3.client("s3")
-    bucket = uri.split('/')[0]
-    url = '/'.join(uri.split('/')[1:])
-
-    if not os.path.isfile(local_path):
-        client.download_file(bucket, url, local_path)
-
-def download_dataset(df):
-    # Download the videos in total before decomp (we usually delete them as frames are extracted) 
-    Because we need to get the fps to calculate sampling rates for decomp
-    with tqdm.tqdm(desc="Downloading PHI videos for entry/exit decomp", total=len(df)) as pbar:
-        with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-            futures = []
-            for uri, local_path in zip(df["origin_uri"].values, df["local_path"].values):
-                # More inefficient than apply but still < 200 vids generally. Slowdown is marginal
-                futures.append(
-                    executor.submit(
-                        download_from_uri,
-                        uri=uri,
-                        local_path=local_path,
-                    )
-                )
-            for future in concurrent.futures.as_completed(futures):
-                pbar.update(1)
-                if future.exception():
-                    print(f"Exception at {future}, {future.exception()}")
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Download S3 URIs and perform frame-level decomp using LightML decomp")
-    parser.add_argument('--feature', action='store_true')
-    parser.add_argument('--no-feature', dest='feature', action='store_false')
-    
-    # Make the folder to download videos to, if it doesn't exist already
-    os.makedirs(LOCAL_DOWNLOADED_VID_PATH, exist_ok=True)
-    # Read in the training csv 
-    df = pd.read_csv(os.path.join(curr, 'training_data.csv'))
+def format_data_csv(path):
+    df = pd.read_csv(path)
 
     # Name start_time / end_time as required by lightml decomp
     df = df.rename(columns={
@@ -60,15 +32,35 @@ if __name__ == "__main__":
         "s3_loc": "origin_uri",
     })
 
+    # Format the s3 uris like in the standard db
+    df["origin_uri"] = df["origin_uri"].apply(lambda x: os.path.join("https://s3.amazonaws.com/", x))
+
     # Name the "value" (label) as in_procedure, since we are just doing a binary problem
     df["value"] = ["in_procedure"]*len(df)
 
     # Define our local path to download uris to 
     df["local_path"] = df["origin_uri"].apply(lambda x: os.path.join(LOCAL_DOWNLOADED_VID_PATH, x.split('/')[-1]))
 
+    # Remove videos that start/end inside colon
+    df = df.dropna(subset=["start_time", "end_time"])
 
-    decomp = DecompFromDataFrame(
-        files=df,
+    # Convert start time from MM:SS to milliseconds for lightml decomp
+    df["start_time"] = df["start_time"].apply(lambda x: convert_to_ms(x))
+    df["end_time"] = df["end_time"].apply(lambda x: convert_to_ms(x))
+
+    return df
+
+
+if __name__ == "__main__":
+    os.makedirs(LOCAL_DOWNLOADED_VID_PATH, exist_ok=True)
+
+    # Format the csv files for use with lightml
+    train = format_data_csv(os.path.join(curr, 'training_data.csv'))
+    holdout = format_data_csv(os.path.join(curr, 'holdout_data.csv'))
+    print(train.apply(lambda x: x["start_time"] < x["end_time"], axis=1))
+    # perform decomp for each video
+    handler = DecompFromDataFrame(
+        files=train,
         frames_per_clip=100,
         frames_per_nonclip=100,
         local_path=os.path.join(curr, 'decompV1'),
@@ -77,3 +69,4 @@ if __name__ == "__main__":
         test_prop=0,
     )
 
+    handler.decomp()
