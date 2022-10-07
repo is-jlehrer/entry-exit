8810c3b58f16bd32695ee29b6110b054bdb8e656
Julian Lehrer <julian.lehrer@iterativescopes.com>
2022-10-07 16:59:51 -0700

    .

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -32,6 +32,9 @@ def generate_dataloaders():
                 transforms.Resize(256),
                 transforms.CenterCrop(224),
                 transforms.RandomHorizontalFlip(),
+                transforms.ColorJitter(),
+                transforms.RandomRotation(),
+                transforms.RandomVerticalFlip(),
                 transforms.ToTensor(),
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
             ]
