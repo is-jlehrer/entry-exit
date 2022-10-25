4184580302ec16e1c84eb5b3865de0c8e1fb475d
Julian Lehrer <julian.lehrer@iterativescopes.com>
2022-10-25 16:05:50 -0700

    .

--- src/models/inference_model.py
+++ src/models/inference_model.py
@@ -136,7 +136,7 @@ class InferenceModel:
                         for idx in false_positives_indices:
                             img = temp_imgs[idx]
                             prob = maxs[idx].item()
-                            img.save(f"false_positive_{fno}_prob_{prob}_vid_{local_path}.png")
+                            img.save(f"false_positive_{fno}_prob_{prob}_vid_{local_path.split('/')[-1]}.png")
                     else:
                         # inside
                         print(f'INSIDE: Time is {time=}, {start_time=}, {end_time=}')
@@ -147,7 +147,7 @@ class InferenceModel:
                             print(f'Writing img {idx}')
                             img = temp_imgs[idx]
                             prob = maxs[idx].item()
-                            img.save(f"false_negative_{fno}_prob_{prob}_vid_{local_path}.png")
+                            img.save(f"false_negative_{fno}_prob_{prob}_vid_{local_path.split('/')[-1]}.png")
                     #########################################################
 
                     preds.extend(out)
