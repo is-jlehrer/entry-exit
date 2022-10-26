f94c9c2eb9ab47593e02f4a61442e92e38b34572
Julian Lehrer <julian.lehrer@iterativescopes.com>
2022-10-26 11:28:51 -0700

    .

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -164,9 +164,9 @@ if __name__ == "__main__":
                 StochasticWeightAveraging(swa_lrs=0.01),
                 savingcallback,
             ],
-            "track_grad_norm": 2,
             "accelerator": "gpu",
             "devices": 1,
+            "limit_val_batches": 10,
         },
         model_config={
             "optimizer": optimizer,
